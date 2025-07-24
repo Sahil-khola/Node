5248c6d3886b74c5016025020b07cac05ff8379d
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-07-24 13:02:57 +0530

    Node

---
+++ 01_Hello-world/hello.js
@@ -0,0 +1,21 @@
+// console.log("hey My name is Sahil Khola");
+
+
+// const math = require("./math");
+const math = require("./math");
+const {addFun,multiFun,subFun} = require("./math");
+
+
+
+// console.log("The Math value",math.add(10,20));
+// console.log("The Math value",math.sub(10,20));
+// console.log("The Math value",math.muntple(10,20));
+
+// console.log(math.addFun(1,2),math.multiFun(20,20),math.subFun(2,3));
+console.log(addFun(2,2),subFun(2,2),multiFun(2,2));
+
+
+
+
+
+
