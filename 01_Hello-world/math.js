65df154d9b1fd837a048f904abe792bf19c6c518
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-07-24 13:10:23 +0530

    Node

--- 01_Hello-world/math.js
+++ 01_Hello-world/math.js
@@ -1,17 +1,21 @@
-function add(a,b){
-    return a+b;
-}
+// function add(a,b){
+//     return a+b;
+// }
 
-function sub(a,b) {
-    return a-b;
-}
+// function sub(a,b) {
+//     return a-b;
+// }
 
-function muntple(a,b) {
-    return a*b;
-}
+// function muntple(a,b) {
+//     return a*b;
+// }
 
-module.exports={
-    addFun : add,
-    subFun :sub,
-    multiFun :muntple,
-}
+// module.exports={
+//     addFun : add,
+//     subFun :sub,
+//     multiFun :muntple,
+// }
+
+// exports.add = (a,b)=>a+b;
+// exports.sub = (a,b)=>a-b;
+// exports.muntple = (a,b)=>a*b;
