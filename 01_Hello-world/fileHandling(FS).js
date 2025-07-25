5c03b036dce695c6eff656c0bfb400c2d6950f35
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-07-25 12:45:03 +0530

    Node

---
+++ 01_Hello-world/fileHandling(FS).js
@@ -0,0 +1,36 @@
+const { log } = require("console");
+const fs = require("fs");
+
+
+
+// fs.writeFileSync("./text.txt","Hello There My Name is Sahil khola ")
+
+// fs.writeFile("./text", "Rao Shabh", (err, res) => {
+//     if (err) {
+//         console.log("error", err);
+//     } else {
+//         console.log(res);
+//     }
+// });
+
+
+
+// const result = fs.readFileSync("text.txt","utf-8");
+// console.log(result);
+
+// fs.readFile("./text.txt", "utf-8", (err, res) => {
+//     if (err) {
+//         console.log("error", err);
+//     } else {
+//         console.log(res);
+//     }
+// });
+
+// const result = fs.appendFileSync("./text.txt","\nEmail : Sahilkhola@124")
+fs.appendFile("text.txt", "\nCity : Gurugram", (err, res) => {
+    if (err) {
+        console.log("error", err);
+    } else {
+        console.log(res);
+    }
+})
