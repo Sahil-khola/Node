d357586a1c4e0df75531b3dcab63790da0c15968
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-07-25 23:40:17 +0530

    Node

---
+++ 02_Server/httpMethod.js
@@ -0,0 +1,7 @@
+// HTTP METHOD -->
+
+// Get  --> read when we get data from server
+// Post  --> create when we send data to server
+// Put   --> update when we send file and imge to server
+// Patch --> update when we update data
+// Delete --> delete when we delete data from server
