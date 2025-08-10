7d5bceef925d799a4ec603f3b133bf64e5082cd1
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-08-10 13:39:56 +0530

    Node

--- Chapter - 1/01_index.js
+++
@@ -1,8 +0,0 @@
-const math = require("./02_Modules");
-console.log(math);
-
-console.log(math.sum(1, 2));
-console.log(math.multiply(1, 2));
-console.log(math.divide(1, 2));
-console.log(math.minus(2,2));
-
