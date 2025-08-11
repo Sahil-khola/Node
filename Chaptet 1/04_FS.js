27d1032c4ab17a1175329d0cc197c3d244df369e
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-08-11 11:00:14 +0530

    Node

--- Chaptet 1/04_FS.js
+++ Chaptet 1/04_FS.js
@@ -2,6 +2,18 @@ const fs = require("fs");
 
 // const txt = fs.readFileSync("demo.txt","utf-8");
 // console.log(txt);
-fs.readFile("demo.txt","utf-8",(err , data)=>{
-console.log(data);
+// fs.readFile("demo.txt","utf-8",(err , data)=>{
+// console.log(data);
+// });
+
+
+
+
+const data = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos eaque molestiae, explicabo doloremque repellendus est error esse iure voluptas nostrum? Tempora sunt similique, qui accuveniam officiis suscipit hic cumque aut. Odit non ducimus odio ab nostrum libero vel neque similique amet. Ad dolorem iusto ratione sapiente ab inventore est."
+fs.writeFile("test.txt", data, "utf-8", (err) => {
+  if (err) {
+    console.log("Error writing file:", err);
+  } else {
+    console.log("File written successfully!");
+  }
 });
