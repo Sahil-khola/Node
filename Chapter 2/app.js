829dd5dbe0181ba7cf8a9c56a3f99b0ce3ddc18b
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-08-11 13:27:34 +0530

    Node

---
+++ Chapter 2/app.js
@@ -0,0 +1,34 @@
+const http = require("http");
+const fs = require("fs");
+
+
+const index = fs.readFileSync("index.html","utf-8");
+const data = fs.readFileSync("data.json","utf-8");
+const server = http.createServer((req,res)=>{
+  console.log(req.url);
+
+  switch (req.url) {
+    case "/":
+        res.setHeader("Content-Type","text/html");
+        res.end(index);
+        break;
+
+    case "/api":
+        res.setHeader("Content-Type","application/json");
+        res.end(data);
+        break;
+         
+    default:
+      res.writeHead(404);
+      res.end();
+      break;
+  }
+
+
+});
+
+const port = 3000;
+server.listen(port,()=>{
+console.log(`http://localhost:${port}`);
+
+})
