d2bb363c53c10f13856f7759ae26b7b13e046296
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-08-19 12:12:15 +0530

    Node

--- 06_Chapter 6/app.js
+++ 06_Chapter 6/app.js
@@ -1,14 +1,18 @@
+const { configDotenv } = require("dotenv");
 const express = require("express");
+require("dotenv").config();
 const app = express();
 
 
 
-app.use(express.static("public"));
+app.use(express.static(process.env.PUBLIC_DIR));
 app.get("/",(req,res)=>{
 // res.send("Sahil  Khola ");
-res.render("index.html");
+
+// res.render("index.html");
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log("Server is running on http://localhost:3000");
+    
 });
